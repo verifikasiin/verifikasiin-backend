e9c1f94252774ebd04644e4b43798c655c9ebc33
Adrian <adrianhakimutomo@gmail.com>
2023-06-03 15:46:41 +0700

    change file name to server.js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/login', (req, res) => {
+    // Authenticate User
+
+    
 })
 
 app.listen(port, () => {
