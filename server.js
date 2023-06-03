bd654cf2cf0ad906b64724a5afb1639aa91bb5b0
Adrian <adrianhakimutomo@gmail.com>
2023-06-03 20:16:23 +0700

    added essential things

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
+require('dotenv').config()
 const express = require("express");
+const {sequelize, testDatabaseConnection} = require('./config/db')
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+
+testDatabaseConnection()
 
 app.use(express.json())
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!')
+app.get('/api/users', (req, res) => {
+    res.json(users)
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/register', async (req, res) => {
     // Authenticate User
-
-    
 })
 
 app.listen(port, () => {
